9e09166fa85b9032b5905a0c4f4ee6a15120b658
Tim Kinsman <40594657+timkinsman@users.noreply.github.com>
2023-10-04 07:58:56 +1030

    Initialize develop

---
+++ src/lib/axios.ts
@@ -0,0 +1,24 @@
+import Axios from 'axios';
+
+import { API_URL } from '@/config';
+import { useNotificationStore } from '@/stores/notifications';
+
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    useNotificationStore.getState().addNotification({
+      type: 'error',
+      title: 'Error',
+      message,
+    });
+
+    return Promise.reject(error);
+  }
+);
