0fec172bbdebbc0246005b76850575a39abbeb4f
timkinsman <tkinsm@gmail.com>
2026-02-26 09:42:03 +1030

    feat: add sentry integration
    experimental/discovery

---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://50b74b4e9c84cb35a9f34a474b260c94@o4510878626545664.ingest.de.sentry.io/4510949088428112",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 // Error boundaries must be Client Components
+import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
 import { useEffect } from "react";
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/error#global-errorjs
@@ -14,7 +16,7 @@ export default function GlobalError({
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error);
+    Sentry.captureException(error);
   }, [error]);
 
   return (
